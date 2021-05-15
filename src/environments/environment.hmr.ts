fd817d78e44a601fa800351d1f34747fc6ec7d73
minhvu1502 <43747199+minhvu1502@users.noreply.github.com>
2021-05-15 21:53:43 +0700

    update

--- src/environments/environment.hmr.ts
+++ src/environments/environment.hmr.ts
@@ -4,9 +4,23 @@
 
 export const environment = {
   SERVER_URL: `./`,
-  production: false,
-  useHash: true,
-  hmr: true,
+  BASE_API_URL: 'http://localhost:8310/',
+  BASE_UPLOAD_URL: 'https://localhost:44375/api/server-upload/upload',
+  BASE_FILE_URL: 'https://localhost:44375/Resources/Images/',
+  ALLOW_ANONYMOUS: '?_allow_anonymous=true',
+  production: true,
+  useHash: false,
+  hmr: false,
+  pro: {
+    theme: 'light',
+    menu: 'side',
+    contentWidth: 'fluid',
+    fixedHeader: true,
+    autoHideHeader: true,
+    fixSiderbar: true,
+    onlyIcon: false,
+    colorWeak: false,
+  },
 };
 
 /*
